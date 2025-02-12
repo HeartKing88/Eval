03d675030d3d8dc65a4f43b268a323a82f459737
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-12 19:14:05 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,7 +8,7 @@ APP_ID = 25742938
 # Get this value from my.telegram.org/apps
 HASH_ID = "b35b715fe8dc0a58e8048988286fc5b6"
 # Get your token from @BotFather on Telegram.
-TOKEN = "7280541678:AAHWXTNqehfQYuYT-WbSOmPKNZJKCdMwm2E"
+TOKEN = "8078694582:AAEfQlQf_MLckMSFzojM62c4Kxa1oqOn3Mw"
 DB_NAME = "shizuDB"
 #databse
 MONGO_URL = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
