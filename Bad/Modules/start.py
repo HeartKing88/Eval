3d209f9b653489101d6faec0e2ca4fc2328ed8ca
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-12 20:57:24 +0530

    Update start.py

--- Bad/Modules/start.py
+++ Bad/Modules/start.py
@@ -4,6 +4,9 @@ from Bad import app
 
 @app.on_message(filters.command("start") & ~filters.forwarded & ~filters.via_bot)
 async def start_command(client, message):
+    # Start command message delete karne ke liye
+    await message.delete()
+
     text = """🐋 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴇᴠᴀʟ ʙᴏᴛ! ✨
 
 📊 ꜰᴏʀ ᴇᴠᴀʟᴜᴀᴛɪᴏɴ ᴀɴᴅ ɪɴꜰᴏ, ꜱɪᴍᴘʟʏ ᴛʏᴘᴇ ʙᴏᴛ ᴀᴄᴛɪᴠɪᴛʏ ʙᴇʟᴏᴡ!
@@ -12,6 +15,8 @@ async def start_command(client, message):
 
     keyboard = InlineKeyboardMarkup([
         [InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/II_BAD_BABY_II")],
+    ],
+    [
         [InlineKeyboardButton("🔔 sᴜᴘᴘᴏʀᴛ", url="https://t.me/PBX_CHAT")],
         [InlineKeyboardButton("🛠 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/HEROKUBIN_01")]
     ])
