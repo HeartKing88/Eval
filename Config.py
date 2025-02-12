ca1f90f4cefcfa944081df42c0dc2359ae49be32
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-12 19:14:18 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,7 +9,7 @@ APP_ID = 25742938
 HASH_ID = "b35b715fe8dc0a58e8048988286fc5b6"
 # Get your token from @BotFather on Telegram.
 TOKEN = "8078694582:AAEfQlQf_MLckMSFzojM62c4Kxa1oqOn3Mw"
-DB_NAME = "shizuDB"
+DB_NAME = "evalDB"
 #databse
 MONGO_URL = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
 LOGGERS_ID = "-1002056907061"
