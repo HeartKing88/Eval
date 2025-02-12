92090db94c9bb543668e64c009f6330e12ad9d37
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-12 18:20:55 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,14 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get this value from my.telegram.org/apps
+APP_ID = 25742938
+# Get this value from my.telegram.org/apps
+HASH_ID = "b35b715fe8dc0a58e8048988286fc5b6"
+# Get your token from @BotFather on Telegram.
+TOKEN = "7280541678:AAHWXTNqehfQYuYT-WbSOmPKNZJKCdMwm2E"
+DB_NAME = "shizuDB"
+#databse
+MONGO_URL = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
