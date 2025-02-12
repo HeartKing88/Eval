2321a11f882587d223ab781a65adaaf9162db067
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-12 20:58:29 +0530

    Update help.py

--- Bad/Modules/help.py
+++ Bad/Modules/help.py
@@ -4,6 +4,9 @@ from Bad import app
 
 @app.on_message(filters.command("help") & ~filters.forwarded & ~filters.via_bot)
 async def help_command(client, message):
+    # Help command message delete karne ke liye
+    await message.delete()
+
     text = """Wᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ʙᴏᴛ! Bᴇʟᴏᴡ ʏᴏᴜ ᴡɪʟʟ ғɪɴᴅ ᴀ ʟɪsᴛ ᴏғ ᴄᴏᴍᴍᴀɴᴅs ʏᴏᴜ ᴄᴀɴ ᴜsᴇ, ᴀʟᴏɴɢ ᴡɪᴛʜ ᴇxᴘʟᴀɴᴀᴛɪᴏɴs ᴀɴᴅ ᴇxᴀᴍᴘʟᴇs ғᴏʀ ᴇᴀᴄʜ.
 
 /eval [expression] ➕: Evaluate a mathematical expression or code snippet.
