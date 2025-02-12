379d3588de7e5df1de642947cfd6b047fb0de643
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-12 21:55:20 +0530

    Update __main__.py

--- Bad/__main__.py
+++ Bad/__main__.py
@@ -2,7 +2,7 @@ import logging
 import asyncio
 import importlib
 from Bad.Modules import ALL_MODULES
-from Bad import app, Bad, Shizu, Sukh
+from Bad import app, Bad, Shizu, Sukh, Jass
 from pyrogram import idle
 from telethon.sessions import StringSession
 import Config
@@ -30,6 +30,7 @@ def LOGGER(name: str) -> logging.Logger:
 async def main():
     await app.start()
     await Bad.start()
+    await Jass.start()  # Start Telegram (python-telegram-bot) Client
 
     # Start Pyrogram User Session if available
     if Config.STRING1:
@@ -65,6 +66,7 @@ async def main():
     # Stop all clients properly
     await app.stop()
     await Bad.disconnect()
+    await Jass.stop()  # Stop Telegram (python-telegram-bot) Client
 
     if Config.STRING1:
         await Shizu.stop()
