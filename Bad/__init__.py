3fca412c16d4109b63a6352da7925ac985e4f81c
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-12 18:47:50 +0530

    Update __init__.py

--- Bad/__init__.py
+++ Bad/__init__.py
@@ -1,23 +1,21 @@
 from pyrogram import Client
 from telethon import TelegramClient
-from telegram.ext import Application
 import Config
 
 # Pyrogram Client
 app = Client(
-    name="app",
-    api_id=Config.APP_ID,
-    api_hash=Config.HASH_ID,
-    bot_token=Config.TOKEN,
-    plugins=dict(root="Bad.Modules")
+             name="app", 
+             api_id=Config.APP_ID, 
+             api_hash=Config.HASH_ID, 
+             bot_token=Config.TOKEN,
+             plugins=dict(root="Bad.Modules")
 )
 
-# Telethon Client
+# Telethon Bot
 Bad = TelegramClient(
-    session="Bad",
-    api_id=Config.APP_ID,
-    api_hash=Config.HASH_ID
-).start(bot_token=Config.TOKEN)
+             session="Bad",  # Add a session name here
+             api_id=Config.APP_ID, 
+             api_hash=Config.HASH_ID
+             ).start(bot_token=Config.TOKEN)
 
-# Telegram (python-telegram-bot) Client
-Sukh = Application.builder().token(Config.TOKEN).build()
+plugins = dict(root="Bad.Modules")
