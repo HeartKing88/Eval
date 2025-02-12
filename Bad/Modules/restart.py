90c37ec17fc71555a23035d655fb906515269190
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-12 18:50:19 +0530

    Create restart.py

---
+++ Bad/Modules/restart.py
@@ -0,0 +1,17 @@
+import os
+import logging
+from Bad import app
+from pyrogram import filters, Client as PyroClient
+from pyrogram.types import Message
+
+# Initialize logger
+logger = logging.getLogger(__name__)
+
+@app.on_message(filters.command("rs") & ~filters.forwarded & ~filters.via_bot)
+async def restart(client: PyroClient, message: Message):
+    reply = await message.reply_text("**🔁 Restarting...**")
+    await message.delete()
+    await reply.edit_text("Successfully Restarted\nPlease wait 1-2 min for loading user plugins...")
+    logger.info("Bot is restarting...")
+    os.system(f"kill -9 {os.getpid()} && python3 -m Bad")
+  
