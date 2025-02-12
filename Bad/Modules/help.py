ece3f38ba5571b559588e6e2b9a8b55bb7b22041
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-12 21:25:08 +0530

    Update help.py

--- Bad/Modules/help.py
+++ Bad/Modules/help.py
@@ -7,11 +7,15 @@ async def help_command(client, message):
     # Help command message delete karne ke liye
     await message.delete()
 
-    text = """Wᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ʙᴏᴛ! Bᴇʟᴏᴡ ʏᴏᴜ ᴡɪʟʟ ғɪɴᴅ ᴀ ʟɪsᴛ ᴏғ ᴄᴏᴍᴍᴀɴᴅs ʏᴏᴜ ᴄᴀɴ ᴜsᴇ, ᴀʟᴏɴɢ ᴡɪᴛʜ ᴇxᴘʟᴀɴᴀᴛɪᴏɴs ᴀɴᴅ ᴇxᴀᴍᴘʟᴇs ғᴏʀ ᴇᴀᴄʜ.
+    text = """Wᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ʙᴏᴛ! Bᴇʟᴏᴡ ʏᴏᴜ ᴡɪʟʟ ғɪɴᴅ ᴀ ʟɪsᴛ ᴏғ ᴄᴏᴍᴍᴀɴᴅs ʏᴏᴜ ᴄᴀɴ ᴜsᴇ, ᴀʟᴏɴɢ ᴡɪᴛʜ ᴇxᴘ[...]
 
 /eval [expression] ➕: Evaluate a mathematical expression or code snippet.
 /sh [command] 💻: Execute a shell command and return its output.
 /install [package_name] 📦: Install a specified package or software.
 /rs 🔄: Restart the bot or service."""
 
-    await message.reply_text(text)
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🔔 sᴜᴘᴘᴏʀᴛ", url="https://t.me/PBX_CHAT"), InlineKeyboardButton("🛠 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/HEROKUBIN_01")]
+    ])
+
+    await message.reply_text(text, reply_markup=keyboard)
