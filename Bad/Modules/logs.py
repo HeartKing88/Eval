0889b17dd7e58ac823518ddffac77890eb0936bd
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-15 22:49:04 +0530

    Update logs.py

--- Bad/Modules/logs.py
+++ Bad/Modules/logs.py
@@ -1,20 +1,41 @@
 import asyncio
 import os
 import socket
+import aiohttp
 from pyrogram import filters
-from Bad import app, Bad
+from Bad import app
+
+BASE = "https://batbin.me/"
 
 async def is_heroku():
     return "heroku" in socket.getfqdn()
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
+async def BadBin(text):
+    resp = await post(f"{BASE}api/v2/paste", data={"text": text})
+    if not resp["success"]:
+        return
+    link = BASE + resp["message"]
+    return link
 
 @app.on_message(filters.command(["getlog", "logs", "getlogs"]))
 async def log_(client, message):
     log_file = "log.txt"
     if os.path.exists(log_file):
         try:
-            await message.reply_document(document=log_file)
+            with open(log_file, "r") as f:
+                log_content = f.read()
+            link = await BadBin(log_content)
+            await message.reply_text(f"Log file uploaded to: {link}")
         except Exception as e:
-            await message.reply_text(f"An error occurred while sending the log file: {str(e)}")
+            await message.reply_text(f"An error occurred while uploading the log file: {str(e)}")
     else:
         await message.reply_text("Log file does not exist.")
-      
