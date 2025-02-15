878864b6112d5129b2c648b17427eef7a02ae7e9
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-15 23:14:36 +0530

    Update logs.py

--- Bad/Modules/logs.py
+++ Bad/Modules/logs.py
@@ -17,4 +17,13 @@ async def log_(client, message):
             await message.reply_text(f"An error occurred while sending the log file: {str(e)}")
     else:
         await message.reply_text("Log file does not exist.")
-      
+
+@app.on_message(filters.command(["cleanlogs"]))
+async def clean_logs(client, message):
+    log_file = "log.txt"
+    try:
+        with open(log_file, "w") as file:
+            file.write("")  # Clear the content of the log file
+        await message.reply_text("Old logs have been cleaned. New logs will be created.")
+    except Exception as e:
+        await message.reply_text(f"An error occurred while cleaning the log file: {str(e)}")
