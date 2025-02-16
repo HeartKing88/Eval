472f23dd06d496f1049754432437b345e95c8d6f
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-02-16 19:22:28 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,10 +8,11 @@ APP_ID = 25742938
 # Get this value from my.telegram.org/apps
 HASH_ID = "b35b715fe8dc0a58e8048988286fc5b6"
 # Get your token from @BotFather on Telegram.
-TOKEN = "8078694582:AAEfQlQf_MLckMSFzojM62c4Kxa1oqOn3Mw"
+TOKEN = "8078694582:AAHO-K-DO-sjwUncykq8x630LYkSqxpDAPY"
 DB_NAME = "evalDB"
 #databse
 MONGO_URL = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
 LOGGER_ID = "-1002056907061"
 STRING1 = ""
 STRING2 = ""
+OWNER_ID = "7009601543"
