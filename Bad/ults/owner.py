073d590576f1a419c4326f4c716b271097ded0ba
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2025-08-29 11:21:53 +0530

    Update owner.py

--- Bad/ults/owner.py
+++ Bad/ults/owner.py
@@ -1,6 +1,8 @@
 from Config import OWNER_ID, SUDOERS
 
-
+async def get_clone_owner(bot_id: int):
+    # yaha apna logic likho
+    return 7548614955  # example owner id
     
 async def is_owner(bot_id, user_id):
     owner_id = await get_clone_owner(bot_id)
